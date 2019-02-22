b3487d28521813f4961cce79433a3b698202e92a
yaichi81ch <yaich1.8ichi@gmail.com>
2019-02-22 15:28:54 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# SeasonDisplayedByGeolocation
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: "User Denied Geolocation" })
+    );
+  }
+
+  renderContent() {
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
